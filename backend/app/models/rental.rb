83cf06846cf002eb67257e2492dc49ccce9d50a6
Amanda Treutler <amandathedev@gmail.com>
2019-08-19 17:52:44 -0500

    HTML boilerplate

--- backend/app/models/rental.rb
+++ backend/app/models/rental.rb
@@ -1,5 +1,5 @@
 class Rental < ApplicationRecord
-    belongs_to :item
-    belongs_to :renter
-    belongs_to :owner
+  belongs_to :item
+  belongs_to :renter
+  belongs_to :owner
 end
---
+++ frontend/src/index.js
@@ -0,0 +1,8 @@
+// URLS
+const rootUrl = "http://localhost:3000"
+const userUrl = `${rootUrl}/users`
+const itemUrl = `${rootUrl}/items`
+const rentersUrl = `${rootUrl}/renters`
+
+// HTML components
+const mainDiv = document.getElementById('main-div')
