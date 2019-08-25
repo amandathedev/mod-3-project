f715ffa77c64db676ff9c4f51be3d5170726c4e8
Amanda Treutler <amandathedev@gmail.com>
2019-08-25 15:10:56 -0500

    Done for now

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,10 +6,53 @@
 
 // HTML components
   const mainDiv = document.getElementById('main-div')
+  const musicCard = document.getElementById('music-card')
+  const audioCard = document.getElementById('audio-card')
+  const photoCard = document.getElementById('photo-card')
+  const tvCard = document.getElementById('tv-card')
+  const sportCard = document.getElementById('sport-card')
+  const craftCard = document.getElementById('craft-card')
+  const homeCard = document.getElementById('home-card')
+  const kidCard = document.getElementById('kid-card')
+
+// Event listeners
+  musicCard.addEventListener('click', () => {
+    console.log('music clicked');
+  })
+
+  audioCard.addEventListener('click', () => {
+    console.log('audio clicked');
+  })
+
+  photoCard.addEventListener('click', () => {
+    console.log('photo clicked');
+  })
+
+  tvCard.addEventListener('click', () => {
+    console.log('tv clicked');
+  })
+
+  sportCard.addEventListener('click', () => {
+    console.log('sport clicked');
+  })
+
+  craftCard.addEventListener('click', () => {
+    console.log('craft clicked');
+  })
+
+  homeCard.addEventListener('click', () => {
+    console.log('home clicked');
+  })
+
+  kidCard.addEventListener('click', () => {
+    console.log('kid clicked');
+  })
+
+// Categories
 
 
-  
-const createCard = () => {
-  const itemCard = document.createElement('div')
-  
-}
+
+// Function calls
+
+
+
--- todos.md
+++ todos.md
@@ -1,9 +1,11 @@
 Build the API √
 Seed data √
-Add model: rentals
+Add model: rentals √
 App name
 Login -- like Netflix? --> Seed user photos
 Logout
 Sign up
 Links
 Add categories with photos
+
+1. Build out the container for the categories in a grid with photos
